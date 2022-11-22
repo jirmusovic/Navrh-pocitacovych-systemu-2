f41f18c5c61405ece5c0838ded0b2aacbe55131b
jirmusovic <jirmusovav@gmail.com>
2022-11-22 11:42:12 +0100

    done

--- hello.s
+++ hello.s
@@ -20,52 +20,51 @@ params_sys5:    .space  8           ; misto pro ulozeni adresy pocatku
 
                 
 main:
-        key_check:
-                daddi   r17, r0, key    ;key v r17
-                lb      r28, 0(r17)
-                daddi   r10, r0, stahp
-                lb      r10, 0(r10)
-                sub     r28, r28, r10
-                daddi   r28, r28, 1
-                ;check
-                sb      r28, 0(r17)
+        key_check:                      ; je potreba zmenit ascii hodnoty klice do rozmezi a-z
+                daddi   r17, r0, key    ; key v r17
+                lb      r28, 0(r17)     ; prvni byte z klice do r28
+                daddi   r10, r0, stahp  ; presouvam az
+                lb      r10, 0(r10)     ; nacteni a do r10
+                sub     r28, r28, r10   ; odecteni hodnoty a od klice
+                daddi   r28, r28, 1     ; pricteni 1 kvuli hranicim
+                sb      r28, 0(r17)     ; ulozeni nove hodnoty klice zpet
 
-                lb      r28, 1(r17)
+                lb      r28, 1(r17)     ; prakticky to same, akorat se z
                 daddi   r10, r0, stahp
                 lb      r10, 0(r10)
                 sub     r28, r28, r10
                 daddi   r28, r28, 1
-                sb      r28, 1(r17)
+                sb      r28, 1(r17)     ; klic je komplet novy
 
 
                 xor     r10, r10, r10
-                xor     r15, r15, r15   ;cisteni registru
+                xor     r15, r15, r15   ; cisteni registru
 
-                daddi   r15, r0, login  ;login v r15
-                daddi   r4, r0, cipher
+                daddi   r15, r0, login  ; login do r15
+                daddi   r4, r0, cipher  ; sifra do r4
 
                 
         magic:
-                jal     is_number       ;overeni cisla + nacteni prvniho znaku loginu
-                bnez    r10, end_magic  ;pokud je overeny znak cislo, vypis zasifrovane slovo a ukonci program
-                daddi   r17, r0, key
-                lb      r17, 0(r17)     
-                lb      r10, 0(r15)
-                add     r28, r10, r17
-                jal     overflow_check
-                sb      r28, 0(r4)
+                jal     is_number       ; overeni cisla + nacteni prvniho znaku loginu
+                bnez    r10, end_magic  ; pokud je overeny znak cislo, vypis zasifrovane slovo a ukonci program
+                daddi   r17, r0, key    ; nahrani klice
+                lb      r17, 0(r17)     ; prvni byte z klice
+                lb      r10, 0(r15)     ; prvni byte z loginu
+                add     r28, r10, r17   ; sectu byte loginu a byte sifry
+                jal     overflow_check  ; stavim abecedy za sebe tak, aby a predchazelo z a z nasledovalo a
+                sb      r28, 0(r4)      ; store
                 
 
 
 
                 daddi   r15, r15, 1
-                daddi   r4, r4, 1     ;inc r17 a r15 (posuneme se na dalsi byte)
-                jal     is_number       ;overeni cisla + nacteni noveho prvniho znaku loginu
-                bnez    r10, end_magic  ;opet koncime magii, pokud narazime na cislo
-                daddi   r17, r0, key
-                lb      r17, 1(r17)     
+                daddi   r4, r4, 1       ; inc r17 a r15 (posuneme se na dalsi byte)
+                jal     is_number       ; overeni cisla + nacteni noveho prvniho znaku loginu
+                bnez    r10, end_magic  ; opet koncime magii, pokud narazime na cislo
+                daddi   r17, r0, key    
+                lb      r17, 1(r17)     ; nacteni druheho bytu z klice
                 lb      r10, 0(r15)
-                sub     r28, r10, r17
+                sub     r28, r10, r17   ; odecteni klice od sudych pismen sifrovaneho slova
                 jal     overflow_check
                 sb      r28, 0(r4)   
 
@@ -74,9 +73,9 @@ main:
 
                 daddi   r15, r15, 1
                 daddi   r4, r4, 1
-                b       magic
+                b       magic           ; loop magie do konce slova
         end_magic:
-                daddi   r4, r0, cipher  ;do r4 nahrajeme vyslednou sifru
+                daddi   r4, r0, cipher  ; do r4 nahrajeme vyslednou sifru
                 jal     print_string    ; vypis pomoci print_string - viz nize
 
 
@@ -84,32 +83,32 @@ main:
 
 
 is_number:
-
+                                                ; check sifrovaneho slova, aby ukoncil akci, kdyz narazi na cislo
                 lb      r28, 0(r15)
-                daddi   r10, r0, stahp   
+                daddi   r10, r0, stahp          ; ohraniceni a-z
                 lb      r10, 0(r10)   
 
-                slt     r10, r28, r10
-                jr      r31
+                slt     r10, r28, r10           ; do r10 nacteme vysledek porovnani
+                jr      r31                     ; jump back
 
 ;;;;;;;NEPREPISOVAT r4 A r15 prosim
-overflow_check:
+overflow_check:                                 ; kruhovy buffer
                 daddi      r10, r0, stahp
                 lb         r10, 0(r10)
                 ;;;;;;;;;;;  1  sif < a
                 ;;;;;;;;;;   0  sif >= a
                 slt        r10, r28, r10
                 beqz       r10, great
-                daddi      r28, r28, 26
+                daddi      r28, r28, 26         ; pricitame, pokud je mensi nez a
 
-        great:  
+        great:                                  
                 daddi      r10, r0, stahp
                 lb         r10, 1(r10)
                 ;;;;;;;;;;;  1   z   < sif
                 ;;;;;;;;;;   0   z   >= sif
                 slt        r10, r10, r28
                 beqz       r10, continue
-                daddi      r28, r28, -26
+                daddi      r28, r28, -26         ; odcitame, pokud je vetsi nez z
 
         continue:
                 jr      r31
@@ -125,3 +124,5 @@ print_string:   ; adresa retezce se ocekava v r4
                 daddi   r14, r0, params_sys5    ; adr pro syscall 5 musi do r14
                 syscall 5   ; systemova procedura - vypis retezce na terminal
                 jr      r31 ; return - r31 je urcen na return address
+
+;end of xjirmu00
